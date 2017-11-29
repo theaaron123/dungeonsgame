dbdcbdea4a295ef5cb1d01e4fbb6b1ef0d3cbb19
chu-lungli <allen.chulung.li@gmail.com>
2017-11-29 22:39:40 +0000

    1.change the way of creating the buttons. 2.Add two button's functions, including "view historical scores" and "choose difficulty level".

--- src/main/java/uk/ac/bath/se/SplashScreen.java
+++ src/main/java/uk/ac/bath/se/SplashScreen.java
@@ -9,28 +9,128 @@ public class SplashScreen {
 
     public SplashScreen() {
 
-        JFrame demo = new JFrame();
-        demo.setSize(400, 150);
-        demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Make a splash screen have three button.
+        JFrame demo1 = new JFrame("Dungeon Game");
+        demo1.setLocationRelativeTo(null);
+        demo1.setVisible(true);
+        demo1.setResizable(false);
 
-        JButton choiceDifficulty = new JButton("Choice Difficulty");
+        demo1.setSize(400, 200);
+        demo1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton choiceDifficulty = new JButton("Choose Difficulty Level");
         JButton startGame = new JButton("Start Game");
-        JButton viewScore = new JButton("View Score");
+        JButton viewScore = new JButton("View Historical Scores");
 
-        demo.getContentPane().add(BorderLayout.NORTH, choiceDifficulty);
-        demo.getContentPane().add(BorderLayout.CENTER, startGame);
-        demo.getContentPane().add(BorderLayout.SOUTH, viewScore);
+        demo1.getContentPane().setLayout(new GridLayout(3,1));
+        demo1.add(choiceDifficulty);
+        demo1.add(startGame);
+        demo1.add(viewScore);
 
-        demo.setVisible(true);
-
+        choiceDifficulty.addActionListener(new choiceDifficulty());
         startGame.addActionListener(new startGameHandler());
+        viewScore.addActionListener(new viewScoreHandler());
     }
-    // choice button-startGame
-    private static class startGameHandler implements ActionListener
+
+    // When player choice button chooseDifficulty, the game shows a new window.
+    public class choiceDifficulty implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            win1 win1 = new win1();
+        }
+    }
+
+    //TODO Those two button still only are button, need to add other function to actually change the difficulty level
+    //TODO For instance, maybe Hell Model have five bot for hunting human player.
+    // The player can choice the level of difficulty of game.
+    public class win1 extends JFrame
     {
-        public void actionPerformed(ActionEvent e)
-        {
+        private win1() {
+
+            JFrame demo2 = new JFrame("Choose Difficulty Level");
+            demo2.setLocationRelativeTo(null);
+            demo2.setVisible(true);
+            demo2.setResizable(false);
+
+            demo2.setSize(400, 200);
+
+            JButton easyModel = new JButton("Easy Model");
+            JButton hellModel = new JButton("Hell Model");
+
+            demo2.getContentPane().setLayout(new GridLayout(2,1));
+            demo2.add(easyModel);
+            demo2.add(hellModel);
+
+            easyModel.addActionListener(new easyModel());
+            easyModel.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e) {
+                    demo2.dispose();
+                }
+            });
+
+            hellModel.addActionListener(new hellModel());
+            hellModel.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e) {
+                    demo2.dispose();
+                }
+            });
+        }
+    }
+
+    // Show a description "You chose Easy Model !" in a new window
+    public static class easyModel implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JFrame f1;
+            f1 =new JFrame("Difficulty Level Chose");
+            f1.setLocationRelativeTo(null);
+            f1.setVisible(true);
+            JOptionPane.showMessageDialog(f1,"You chose Easy Model !");
+            f1.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        }
+    }
+
+    // Show a description "You chose Hell Model !" in a new window
+    public static class hellModel implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JFrame f2;
+            f2 =new JFrame("Difficulty Level Chose");
+            f2.setLocationRelativeTo(null);
+            f2.setVisible(true);
+            JOptionPane.showMessageDialog(f2,"You chose Hell Model !");
+            f2.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        }
+    }
+
+    // When player choice button startGame, the game start.
+    public class startGameHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
             DungeonView d = new DungeonView();
+        }
+    }
+
+    //When player choice button View Historical Scores, the game shows a new window.
+    public class viewScoreHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            win2 win2 = new win2();
+        }
+    }
+
+    // Show Historical Scores in a new window.
+    public class win2 extends JFrame
+    {
+        public win2() {
+
+            JFrame demo3 = new JFrame("Historical Scores");
+            demo3.setLocationRelativeTo(null);
+            demo3.setVisible(true);
+            demo3.setResizable(false);
+            demo3.setSize(400, 400);
+
+            JTextArea scoreArea = new JTextArea();
+
+            demo3.getContentPane().add(BorderLayout.CENTER, scoreArea);
+
+            scoreArea.append("Player Name:" + "Gold:\t" + "Score:\n" + Player.getTopPlayer());
+
         }
     }
 }
