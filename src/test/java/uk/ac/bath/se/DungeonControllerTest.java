7420ac5b8188a4e677542e7c8da491329a1f06ed
Aaron <aaronjsunk@gmail.com>
2017-12-07 19:19:28 +0000

    JDBC driver load tested, player movement tested in all directions, GamePlayLookUpTest methods adhering to Kent Becks naming convention

--- src/test/java/uk/ac/bath/se/DungeonControllerTest.java
+++ src/test/java/uk/ac/bath/se/DungeonControllerTest.java
@@ -2,9 +2,49 @@ package uk.ac.bath.se;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class DungeonControllerTest {
+    @Test
+    public void testMovePlayerRight() throws Exception {
+        DungeonController dungeonController = new DungeonController();
+        dungeonController.initialiseDungeonGame();
+        int xCoordInitial = dungeonController.player.getxCoord();
+        dungeonController.movePlayer(PlayerMovement.RIGHT);
+        int xCoord = dungeonController.player.getxCoord();
+        assertTrue(xCoord == (xCoordInitial + 1));
+    }
+
+    @Test
+    public void testMovePlayerLeft() throws Exception {
+        DungeonController dungeonController = new DungeonController();
+        dungeonController.initialiseDungeonGame();
+        int xCoordInitial = dungeonController.player.getxCoord();
+        dungeonController.movePlayer(PlayerMovement.LEFT);
+        int xCoord = dungeonController.player.getxCoord();
+        assertTrue(xCoord == (xCoordInitial - 1));
+    }
+
+    @Test
+    public void testMovePlayerUp() throws Exception {
+        DungeonController dungeonController = new DungeonController();
+        dungeonController.initialiseDungeonGame();
+        int yCoordInitial = dungeonController.player.getyCoord();
+        dungeonController.movePlayer(PlayerMovement.UP);
+        int yCoord = dungeonController.player.getyCoord();
+        assertTrue(yCoord == (yCoordInitial - 1));
+    }
+
+    @Test
+    public void testMovePlayerDown() throws Exception {
+        DungeonController dungeonController = new DungeonController();
+        dungeonController.initialiseDungeonGame();
+        int yCoordInitial = dungeonController.player.getyCoord();
+        dungeonController.movePlayer(PlayerMovement.DOWN);
+        int yCoord = dungeonController.player.getyCoord();
+        assertTrue(yCoord == (yCoordInitial + 1));
+    }
+
     @Test
     public void addDungeonBounds() throws Exception {
     }
